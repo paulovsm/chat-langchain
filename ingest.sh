d6f074e7d776ec0373ea070a74c5ec7114431a25
paulovsm <paulovsm@gmail.com>
2023-05-22 13:31:31 +0000

    Fixing playwright dependency

--- ingest.sh
+++ ingest.sh
@@ -2,5 +2,8 @@
 # This involves scraping the data from the web and then cleaning up and putting in Weaviate.
 # Error if any command fails
 set -e
+pip install playwright > /dev/null
+pip install  lxml
+playwright install
 python ingest_url.py
 python ingest_docs.py
